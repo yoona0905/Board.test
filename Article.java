d2a70086d3254b372f186735d8908fd01df65b49
yoona0905 <dbsgur511@naver.com>
2021-11-15 16:55:48 +0900

    간 단 하 게 정 리

---
+++ Article.java
@@ -0,0 +1,18 @@
+package board;
+
+public class Article {
+
+	int id;
+	String title;
+	String body;
+
+
+	public Article(int id, String title, String body) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.body = body;
+	}
+	
+	
+}
--- Board.java
+++ Board.java
@@ -4,11 +4,10 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Board {
-	ArrayList<String> titles = new ArrayList<>();
-	ArrayList<String> bodies = new ArrayList<>();
-	ArrayList<Integer> numbers = new ArrayList<>();
+	ArrayList<Article> articles = new ArrayList<>();
 	int no = 1; // 게시물 번호
 	// Integer은 숫자
+	Scanner sc = new Scanner(System.in);
 
 	public void runBoard() {
 
@@ -17,80 +16,54 @@ public class Board {
 		// list : 게시물 목록 조회 (출력)
 		// 명령어를 입력해주세요 : (출력) add(입력)
 		
-		Scanner sc = new Scanner(System.in);
 		
 		while(true) {
 			System.out.println("명령어를 입력해주세요 : ");
-			String cmd = sc.nextLine();
-			
-			
+			String cmd = sc.nextLine();				
 			if(cmd.equals("help")) {
-				System.out.println("add : 게시물 등록");
-				System.out.println("list : 게시물 목록 조회");
-				System.out.println("update : 게시물 수정");
-				System.out.println("delete : 게시물 삭제");
+				printHelp();
 			}else if(cmd.equals("add")) {
-				
 				// 중복되지 않아야 된다. 자동으로 만들어 줘야 된다.
-				
-				
-				numbers.add(no);
-				System.out.println("제목 입력해주세요 : ");
-				String title = sc.nextLine();
-				System.out.println("내용을 입력해주세요 : ");
-				String body = sc.nextLine();
-				
-				titles.add(title);
-				bodies.add(body);
-				
-				System.out.println("게시물이 저장되었습니다.");	
-				no++; // 번호 자동 증가
+				addArticle();	
 			}else if(cmd.equals("list")) {
 				list();
 
 			}else if(cmd.equals("update")) {
-				System.out.println("수정할 게시물 번호: ");
-				int targetNo = Integer.parseInt(sc.nextLine());
 				
-				int targetIndex = getIndexOfArticleNo(targetNo);
-			
-				if(targetIndex == -1) {
-					System.out.println("없는 게시물입니다.");
-				}else{
-					System.out.println("새제목 : ");
-					String title = sc.nextLine();
-					System.out.println("새내용 : ");
-					String body = sc.nextLine();
-
-					titles.set(targetIndex, title);
-					bodies.set(targetIndex, body);
-					System.out.println("수정이 완료되었습니다.");
-					list();
-				}	
+				updateArticle();
+				
 			} 		else if(cmd.equals("delete")) {
-				System.out.println("삭제할 게시물 번호: ");
-				int targetNo = Integer.parseInt(sc.nextLine());
-				
-				int targetIndex = getIndexOfArticleNo(targetNo);
-			
-				if(targetIndex == -1) {
-					System.out.println("없는 게시물입니다.");
-				}else{
-					titles.remove(targetIndex);
-					bodies.remove(targetIndex);
-					numbers.remove(targetIndex);
-					System.out.println("삭제가 완료되었습니다.");
-					list();
-				}	
+				deleteArticle();
 			}
 		}	
 	}
 	
+
+	
+	
+	
+	private void deleteArticle() {
+		System.out.println("삭제할 게시물 번호: ");
+		int targetNo = Integer.parseInt(sc.nextLine());
+		
+		int targetIndex = getIndexOfArticleNo(targetNo);
+	
+		if(targetIndex == -1) {
+			System.out.println("없는 게시물입니다.");
+		}else{
+			articles.remove(targetIndex);
+			System.out.println("삭제가 완료되었습니다.");
+			list();
+		}	
+		
+	}
+
+
 	public int getIndexOfArticleNo(int targetNo) {
 		
-		for(int i = 0; i < numbers.size(); i++) {
-			int currentNo = numbers.get(i);
-			if(targetNo == currentNo){
+		for(int i = 0; i < articles.size(); i++) {
+			Article currentArticle = articles.get(i);
+			if(targetNo == currentArticle.id){
 				return i;
 			}
 			
@@ -101,18 +74,62 @@ public class Board {
 	
 	
 	
-	public void list() {
-		for(int i = 0; i < titles.size();i++) {
+	private void list() {
+		for(int i = 0; i < articles.size();i++) {
 			
-			int ano = numbers.get(i);
-			String title = titles.get(i);
-			String body = bodies.get(i);
+			Article article = articles.get(i);
 			
-			System.out.println("번호 : " + ano);
-			System.out.println("제목 : " + title);
-			System.out.println("내용 : " + body);
+			System.out.println("번호 : " + article.id);
+			System.out.println("제목 : " + article.title);
+			System.out.println("내용 : " + article.body);
 			System.out.println("=====================");
 		}
 	}
+	
+	private void printHelp() {
+		System.out.println("add : 게시물 등록");
+		System.out.println("list : 게시물 목록 조회");
+		System.out.println("update : 게시물 수정");
+		System.out.println("delete : 게시물 삭제");
+		System.out.println("search : 게시물 검색");
+	}
+	
+	private void addArticle() {
+		System.out.println("제목 입력해주세요 : ");
+		String title = sc.nextLine();
+		System.out.println("내용을 입력해주세요 : ");
+		String body = sc.nextLine();
+
+		Article article = new Article(no, title, body);
+		
+		articles.add(article);
+		
+		System.out.println("게시물이 저장되었습니다.");	
+		no++; // 번호 자동 증가
+	}
+	
+	private void updateArticle() {
+		System.out.println("수정할 게시물 번호: ");
+		int targetNo = Integer.parseInt(sc.nextLine());
+		
+		int targetIndex = getIndexOfArticleNo(targetNo);
+	
+		if(targetIndex == -1) {
+			System.out.println("없는 게시물입니다.");
+		}else{
+			System.out.println("새제목 : ");
+			String title = sc.nextLine();
+			System.out.println("새내용 : ");
+			String body = sc.nextLine();
+
+			Article article = new Article(targetNo, title, body);
+			
+			articles.set(targetIndex, article);
+			
+			System.out.println("수정이 완료되었습니다.");
+			list();
+		}	
+	}
+	
 
 }
